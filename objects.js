038930f5d8d059523f9b41ed5f4bb6f3ad5feae9
grahk93 <grace.juster@flatironschool.com>
2016-07-20 17:59:15 -0400

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {Me: "my song"};
+
+
+function updatePlaylist(obj,key,value) {
+    obj[key] = value; 
+    return obj
+}
+
+function removeFromPlaylist(obj,key) {
+  delete obj[key];
+  return obj
+}
